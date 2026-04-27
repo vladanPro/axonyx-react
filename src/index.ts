fbddb20347f96a6a2911e59fac6746d2b9810bee
Vladan Profirovic <vladan.profirovic@gmail.com>
2026-04-27 21:11:49 +0200

    Export Tabs and Dialog

--- src/index.ts
+++ src/index.ts
@@ -4,3 +4,5 @@ export * from "./components/Badge";
 export * from "./components/Alert";
 export * from "./components/Stack";
 export * from "./components/Cluster";
+export * from "./components/Tabs";
+export * from "./components/Dialog";
